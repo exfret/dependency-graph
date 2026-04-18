55ce7e3dcd597992afff012fd1f4755bc753eddb
exfret (kraken) <hess.kyler@gmail.com>
2026-04-18 14:01:31 -0700

    Begin work on damage type node

--- lib/logic/concrete.lua
+++ lib/logic/concrete.lua
@@ -52,16 +52,18 @@ concrete.build = function()
         -- Can we encounter this asteroid chunk?
 
         -- Edges from locations where this chunk spawns naturally
-        for _, place in pairs(lu.asteroid_to_places[key("asteroid-chunk", chunk.name)]) do
+        for _, place in pairs(tablize(lu.asteroid_to_places[key("asteroid-chunk", chunk.name)])) do
             add_edge(place.type, place.name)
         end
-        -- Edges from entities that spawn this chunk when dying
-        -- TODO: Implement after I figure out triggers situation better
-        --[[for _, source in pairs(tablize(lu.dying_spawns_reverse[key("asteroid-chunk", chunk.name)])) do
-            if source.type == "entity" then
-                add_edge("entity-kill", source.name)
-            end
-        end]]
+        -- Edges from triggers that spawn this entity
+        -- Includes dying spawns
+        local stop = key("asteroid-chunk", chunk.name)
+        for struct_ind, _ in pairs(tablize(lu.stop_to_triggers[stop])) do
+            local struct = lu.triggers[struct_ind]
+            add_edge(struct.start_type, struct.start_name, {
+                desc = struct.edge_desc,
+            })
+        end
 
         ----------------------------------------
         add_node("asteroid-chunk-mine", "AND")
@@ -79,13 +81,32 @@ concrete.build = function()
     -- Damage Type
     ----------------------------------------------------------------------
 
-    -- TODO
+    set_class("damage-type")
+
+    for _, damage in pairs(prots("damage-type")) do
+        set_prot(damage)
+
+        ----------------------------------------
+        add_node("damage-type", "OR")
+        ----------------------------------------
+        -- Can we deal damage of this type?
+
+        -- Edges from triggers to damage dealt of this type
+        local stop = key("damage-type", damage.name)
+        for struct_ind, _ in pairs(tablize(lu.stop_to_triggers[stop])) do
+            local struct = lu.triggers[struct_ind]
+            add_edge(struct.start_type, struct.start_name, {
+                desc = struct.edge_desc,
+            })
+        end
+        -- TODO: Impact damage
+    end
 
     ----------------------------------------------------------------------
     -- Entity
     ----------------------------------------------------------------------
 
-    --[[set_class("entity")
+    set_class("entity")
 
     for _, entity in pairs(base_prots("entity")) do
         set_prot(entity)
@@ -98,18 +119,58 @@ concrete.build = function()
         -- TODO: Should having to build an entity turn off automated?
         -- TODO: Maybe think more about automated along with the other ones more. 
         -- TODO: Only add entity-build if the entity is buildable (or when optimization is turned off in settings)
-        add_edge("entity-build")
-        for room_key, _ in pairs(tablize(lu.rooms_spawning_entity[entity.name])) do
+        --[[local buildables = lu.buildables[key(entity)]
+        if buildables ~= nil then
+            add_edge("entity-build")
+        end]]
+        -- TODO: Autoplace
+        --[[for room_key, _ in pairs(tablize(lu.autoplaceable_to_rooms[entity.name])) do
             -- Technically, we should check that there are non-colliding tiles too, but it would be very silly to have an entity in autoplace that can't be placed somewhere
-            add_edge("room-autoplace", room_key, {
+            add_edge("room", room_key, {
                 entity = entity.name,
-                abilities = { ["automated"] = true },
-            }) -- Being directly from a room leads to isolated
+                abilities = { ["isolated"] = true },
+            })
+        end]]
+        for entity_with_corpse_name, _ in pairs(tablize(lu.entities_with_corpse[entity.name])) do
+            add_edge("entity-kill", entity_with_corpse_name)
         end
-        for other_entity_name, _ in pairs(tablize(lu.entities_with_corpse[entity.name])) do
-            add_edge("entity-kill", other_entity_name)
+        for spawner_name, _ in pairs(tablize(lu.spawners_with_capture_result[entity.name])) do
+            add_edge("entity-capture-spawner", spawner_name)
         end
-    end]]
+        -- TODO: Trigger creations, primarily ammo spawns, dying spawns, and capsules
+        -- Asteroid spawning
+        for _, place in pairs(tablize(lu.asteroid_to_places[key("entity", entity.name)])) do
+            add_edge(place.type, place.name)
+        end
+        -- Starting character
+        if entity.type == "character" and entity.name == "character" then
+            add_edge("starting-character", "")
+        end
+
+        --[[if buildables ~= nil then
+            ----------------------------------------
+            add_node("entity-build", "AND")
+            ----------------------------------------
+            -- Can we build this entity using an item?
+            -- Entities that can be planted are counted as being built, though later during randomization we might have to condition on it being a planted or built entity
+
+            add_edge("entity-build-item")
+            add_edge("entity-build-tile")
+            local build_room_restrictions = lu.build_room_restrictions[entity.name]
+            if build_room_restrictions ~= nil then
+                add_edge("entity-build-surface-condition")
+            end
+            if categories.rolling_stock[entity.type] then
+                add_edge("entity-build-rail")
+            end
+
+            ----------------------------------------
+            add_node("entity-build-item", "OR")
+            ----------------------------------------
+            -- Can we get an item needed to build this entity?
+            -- TODO
+        end]]
+    end
 end
 
 return concrete
